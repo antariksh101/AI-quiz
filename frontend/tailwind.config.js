e58c0f50d8c19673e6e52d6f42e6c0c53284221d
antariksh101 <antarikshkahyap157@gmail.com>
2025-10-04 00:01:13 +0530

    tailwind updated

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", // enables dark mode using a class
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // React src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
